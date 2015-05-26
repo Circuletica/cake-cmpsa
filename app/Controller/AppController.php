86d93c8bf221a92772f4c50844d50888f5f32a7d
eBug <ericvb69@gmail.com>
2015-05-26 13:56:36 +0200

    Volvemos a cargar el componente Session

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -31,8 +31,8 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
-	public $components = array('RequestHandler');
-	/*public $components = array('DebugKit.Toolbar','Session');*/
+	//public $components = array('Session','RequestHandler');
+	public $components = array('DebugKit.Toolbar','Session','RequestHandler');
 	public function iban($codigoPais,$ccc){
 	  $pesos = array('A' => '10',
 	 		'B' => '11',
