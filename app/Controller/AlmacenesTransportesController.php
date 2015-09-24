eb424a4b7c1779a14ee8a98d28a9743895faf217
Rodol <rodolgl@gmail.com>
2015-09-24 09:31:28 +0200

    Se modifica controlador almacenestransporte
    Por solucionar Error: SQLSTATE[23000]: Integrity constraint violation:
    1452 Cannot add or update a child row: a foreign key constraint fails

--- app/Controller/AlmacenesTransportesController.php
+++ app/Controller/AlmacenesTransportesController.php
@@ -10,29 +10,21 @@ class AlmacenesTransportesController extends AppController {
 
 	public function add() {
 
-	if($this->request->is('post')){
-		if (!$this->params['named']['from_id']) {
-			$this->Session->setFlash('URL mal formado AlmacenesTransportes/add '.$this->params['named']['from']);
-			$this->redirect(array(
-				'controller' => $this->params['named']['from'],
-				'action' => 'view'));
-		}
-	}
-
-	if($this->AlmacenesTransporte->save($this->request->data)):
-				$this->Session->setFlash('Cuenta corriente almacén guardada');
+	if($this->request->is('post')):
+		$this->request->data['AlmacenesTransporte']['transporte_id'] = $this->params['named']['from_id'];
+			if($this->AlmacenesTransporte->save($this->request->data) ):
+				$this->Session->setFlash('Cuenta corriente almacén guardada guardada');
 				$this->redirect(array(
 					'controller' => 'transportes',
 					'action' => 'view',
 					$this->params['named']['from_id']));
 	endif;
+		endif;
 
 	$this->set('almacenes', $this->AlmacenesTransporte->Almacen->find('list', array(
 	'fields' => array('Almacen.id','Empresa.nombre_corto'),
 	'recursive' => 1))
 	);	
-
-	$this->set('almacenestransportes', $this->AlmacenesTransporte->Almacen->find('list'));
 
 	}
 
--- app/Model/AlmacenesTransporte.php
+++ app/Model/AlmacenesTransporte.php
@@ -1,5 +1,9 @@
 <?php
 class AlmacenesTransporte extends AppModel {
-	public $belongsTo = array('Almacen','Transporte');
+	public $belongsTo = array('Almacen',
+			'Transporte' => array(
+			'className' => 'Transporte',
+			'foreignKey' => 'transporte_id')
+		);
 }
 ?>
