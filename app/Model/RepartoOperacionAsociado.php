43d929ff7256cb75d2af26cdc6c42ab8d1b9f130
eBug <ericvb69@gmail.com>
2015-10-28 23:42:06 +0100

    Aparece el nombre del asociado en Financiacion::view()

--- app/Model/RepartoOperacionAsociado.php
+++ app/Model/RepartoOperacionAsociado.php
@@ -1,6 +1,11 @@
 <?php
 class RepartoOperacionAsociado extends AppModel {
 	public $recursive = 1;
+	public $belongsTo = array(
+		'Asociado' => array(
+			'className' => 'Asociado',
+			'foreignKey' => 'asociado_id')
+	);
 //	public $belongsTo = array(
 //		'Financiacion' => array(
 //			'className' => 'Financiacion',
