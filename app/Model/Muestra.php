ff7a77fa99be7450dd3892bd963505a4a4f63110
eBug <ericvb69@gmail.com>
2015-11-16 00:53:17 +0100

    closes #134 Corregido LineaMuestras::view() para que funcione con Common/view.ctp

--- app/Model/Muestra.php
+++ app/Model/Muestra.php
@@ -1,29 +1,22 @@
 <?php
 class Muestra extends AppModel {
 	public $recursive = 2;
-//	public $belongTo = array('Empresa' => array(
 	public $displayField = 'referencia';
-	//no sabemos si este sirve de algo
-	//public $actsAs = array('Containable');
 	public $belongsTo = array(
-		//no estoy seguro de si hace falta incluir Calidad,
-		//si ya tenemos el enlace con CalidadNombre
-		'Calidad' => array(
-			'className' => 'Calidad',
-			'foreignKey' => 'calidad_id'),
 		'CalidadNombre' => array(
 			'className' => 'CalidadNombre',
 			'foreignKey' => 'calidad_id'),
 		'Proveedor' => array(
 			'className' => 'Proveedor',
 			'foreignKey' => 'proveedor_id'),
-		'MarcaAlmacen' => array(
-			'className' => 'MarcaAlmacen',
-			'foreignKey' => 'marca_almacen_id'),
+		//Quitamos esta hasta que se solucione
+		//en la BDD
+		//'MarcaAlmacen' => array(
+		//	'className' => 'MarcaAlmacen',
+		//	'foreignKey' => 'marca_almacen_id'),
 		'Operacion' => array(
 			'className' => 'Operacion',
 			'foreignKey' => 'operacion_id')
 	);
 	public $hasMany = 'LineaMuestra';
 }
-
