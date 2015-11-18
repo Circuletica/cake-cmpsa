d56feadad7f1eb16136692f75dd59007eabd662f
eBug <ericvb69@gmail.com>
2015-11-19 00:55:54 +0100

    closes #110 Añadidos botones de editar/borrar valores en TipoIva::view() add.ctp y edit.ctp se fusionan en form.ctp

--- app/Controller/ValorTipoIvasController.php
+++ app/Controller/ValorTipoIvasController.php
@@ -10,27 +10,79 @@ class ValorTipoIvasController extends AppController {
 	$this->set('valor_tipo_ivas', $this->paginate());
     }
 
+//    public function add() {
+//	//el id y la clase de la entidad de origen vienen en la URL
+//	$from_id = $this->params['named']['from_id'];
+//	//necesitamos el nombre del flete para el breadcrumb y el título de la vista
+//	$tipo_iva = $this->ValorTipoIva->TipoIva->find('first',
+//	    array(
+//		'conditions' => array('TipoIva.id' => $from_id),
+//		'recursive' => -1
+//	    ));
+//	$this->set(compact('tipo_iva'));
+//	if($this->request->is('post')):
+//	    $this->request->data['ValorTipoIva']['tipo_iva_id'] = $from_id;
+//	    if($this->ValorTipoIva->save($this->request->data)):
+//		$this->Session->setFlash('Valor IVA guardado');
+//		$this->redirect(array(
+//		    'controller' => 'tipo_ivas',
+//		    'action' => 'view',
+//		    $from_id
+//		));
+//	    endif;
+//	endif;
+//    }
+
     public function add() {
-	//el id y la clase de la entidad de origen vienen en la URL
-	$from_id = $this->params['named']['from_id'];
+	//$this->form($this->params['named']['from_id']);
+	$this->form();
+	$this->render('form');
+    }
+
+    public function edit($id = null) {
+	if (!$id && empty($this->request->data)) {
+	    $this->Session->setFlash('error en URL');
+	    $this->redirect(array(
+		'action' => 'view',
+		'controller' => $this->params['named']['from_controller'],
+		$this->params['named']['from_id']
+	    ));
+	}
+	$this->form($id);
+	$this->render('form');
+    }
+
+    public function form($id = null) { //esta acción vale tanto para edit como add
+	//según es un add o edit, cambia el texto del formulario
+	$this->set('action', $this->action);
 	//necesitamos el nombre del flete para el breadcrumb y el título de la vista
 	$tipo_iva = $this->ValorTipoIva->TipoIva->find('first',
 	    array(
-		'conditions' => array('TipoIva.id' => $from_id),
+		'conditions' => array('TipoIva.id' => $this->params['named']['from_id']),
 		'recursive' => -1
 	    ));
-	$this->set(compact('tipo_iva'));
-	if($this->request->is('post')):
-	    $this->request->data['ValorTipoIva']['tipo_iva_id'] = $from_id;
-	    if($this->ValorTipoIva->save($this->request->data)):
-		$this->Session->setFlash('Valor IVA guardado');
+	//$this->set(compact('tipo_iva'));
+	$this->set('tipo_iva', $tipo_iva['TipoIva']);
+
+	//si es un edit, hay que rellenar el id, ya que
+	//si no se hace, al guardar el edit, se va a crear
+	//un _nuevo_ registro, como si fuera un add
+	if (!empty($id)) $this->ValorTipoIva->id = $id; 
+	if(!empty($this->request->data)) { //la vuelta de 'guardar' el formulario
+	    $this->request->data['ValorTipoIva']['tipo_iva_id'] = $this->params['named']['from_id'];
+	    if($this->ValorTipoIva->save($this->request->data)){
+		$this->Session->setFlash('Valor de IVA guardada');
 		$this->redirect(array(
-		    'controller' => 'tipo_ivas',
 		    'action' => 'view',
-		    $from_id
+		    'controller' => $this->params['named']['from_controller'],
+		    $this->params['named']['from_id']
 		));
-	    endif;
-	endif;
+	    } else {
+		$this->Session->setFlash('Valor NO guardado');
+	    }
+	} else { //es un edit, hay que pasar los datos ya existentes
+	    $this->request->data = $this->ValorTipoIva->read(null, $id);
+	}
     }
 
     public function delete($id = null) { 
--- app/View/ValorTipoIvas/form.ctp
+++ app/View/ValorTipoIvas/form.ctp
@@ -1,14 +1,22 @@
-<h2>Agregar valor a IVA <em><?php echo $tipo_iva['TipoIva']['nombre']?></em></h2>
 <?php
 $this->Html->addCrumb('Tipos de IVA','/'.$this->params['named']['from_controller']);
-$this->Html->addCrumb('IVA '.$tipo_iva['TipoIva']['nombre'],'/'.$this->params['named']['from_controller'].'/view/'.$this->params['named']['from_id']);
+$this->Html->addCrumb('IVA '.$tipo_iva['nombre'],'/'.$this->params['named']['from_controller'].'/view/'.$this->params['named']['from_id']);
+
+if ($action == 'add') {
+    echo "<h2>Añadir Valor a Tipo de IVA <em>".$tipo_iva['nombre']."</em></h2>\n";
+}
+
+if ($action == 'edit') {
+    echo "<h2>Modificar Valor de Tipo de IVA <em>".$tipo_iva['nombre']."</em></h2>\n";
+}
+
 echo $this->Form->create();
 echo "<fieldset>\n";
 echo "<div class='linea'>\n";
 echo $this->Form->input('fecha_inicio', array(
     'label'=>'Fecha de Validez',
     'dateFormat' => 'DMY',
-    'minYear' => date('Y')-1,
+    'minYear' => date('Y')-25,
     'maxYear' => date('Y')+5,
     'orderYear' => 'asc'
     ));
@@ -17,7 +25,7 @@ echo "<div class='linea'>\n";
 echo $this->Form->input('fecha_fin', array(
     'label'=>'Fecha de Caducidad',
     'dateFormat' => 'DMY',
-    'minYear' => date('Y')-1,
+    'minYear' => date('Y')-25,
     'maxYear' => date('Y')+5,
     'orderYear' => 'asc',
     'empty' => true
