83e47bab5fbec7a0b9a1662df5e63f8015994c4d
eBug <ericvb69@gmail.com>
2015-11-30 14:15:53 +0100

    Formateo

--- app/Model/Transporte.php
+++ app/Model/Transporte.php
@@ -1,45 +1,44 @@
 <?php
 class Transporte extends AppModel {
-	public $recursive = 3;
- 	public $validate = array(
-    	'nombre_vehiculo' => array(
-      	'rule' => 'notEmpty',
-     	'message' => 'El nombre del vehículo no puede estar vacío'
-      ),
-  	  'matricula' => array(
-      'rule' => 'notEmpty',
-      'message' => 'El BL/matrícula no puede estar vacío'
-      )
+    public $recursive = 3;
+    public $validate = array(
+	'nombre_vehiculo' => array(
+	    'rule' => 'notEmpty',
+	    'message' => 'El nombre del vehículo no puede estar vacío'
+	),
+	'matricula' => array(
+	    'rule' => 'notEmpty',
+	    'message' => 'El BL/matrícula no puede estar vacío'
+	)
     );
 
-	public $belongsTo = array(
-			'Aseguradora' => array(
-				'className' => 'Aseguradora',
-				'foreignKey' => 'aseguradora_id'),
-			'Operacion' => array(
-				'className' => 'Operacion',
-				'foreignKey' => 'operacion_id'),
-			'Naviera' => array(
-				'className' => 'Naviera',
-				'foreignKey' => 'naviera_id'),
-			'PuertoCarga' => array(
-				'className' => 'Puerto',
-				'foreignKey' => 'puerto_carga_id'),
-			'PuertoDestino' => array(
-				'className' => 'Puerto',
-				'foreignKey' => 'puerto_destino_id'),
-			'Agente' => array(
-				'className' => 'Agente',
-				'foreignKey' => 'agente_id')
+    public $belongsTo = array(
+	'Aseguradora' => array(
+	    'className' => 'Aseguradora',
+	    'foreignKey' => 'aseguradora_id'),
+	'Operacion' => array(
+	    'className' => 'Operacion',
+	    'foreignKey' => 'operacion_id'),
+	'Naviera' => array(
+	    'className' => 'Naviera',
+	    'foreignKey' => 'naviera_id'),
+	'PuertoCarga' => array(
+	    'className' => 'Puerto',
+	    'foreignKey' => 'puerto_carga_id'),
+	'PuertoDestino' => array(
+	    'className' => 'Puerto',
+	    'foreignKey' => 'puerto_destino_id'),
+	'Agente' => array(
+	    'className' => 'Agente',
+	    'foreignKey' => 'agente_id')
 	);
 
-	public $hasMany = array(
-		'AlmacenTransporte'=> array(
-			'className' => 'AlmacenTransporte',
-			'foreignKey' => 'transporte_id')
+    public $hasMany = array(
+	'AlmacenTransporte'=> array(
+	    'className' => 'AlmacenTransporte',
+	    'foreignKey' => 'transporte_id')
 	);
 
 
 }
-
 ?>
