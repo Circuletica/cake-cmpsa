63d64839c1e5fb9d9dee5b5a36c98520b214ab32
eBug <ericvb69@gmail.com>
2016-06-07 16:36:41 +0200

    formateo

--- app/Model/Transporte.php
+++ app/Model/Transporte.php
@@ -1,20 +1,19 @@
 <?php
 class Transporte extends AppModel {
-
-	public $recursive = 3;
- 	public $validate = array(
-    	'nombre_vehiculo' => array(
-      	'rule' => 'notBlank',
-     	'message' => 'El nombre del vehículo no puede estar vacío'
-      ),  	
- 	 	'cantidad_embalaje' => array(
-      	'rule' => 'notBlank',
-      	'message' => 'La cantidad de bultos no puede estar vacía'
-      ),
- 	   	'linea' => array(
-      	'rule' => 'notBlank',
-      	'message' => 'La linea de transporte no puede estar vacía'
-      )	
+    public $recursive = 3;
+    public $validate = array(
+	'nombre_vehiculo' => array(
+	    'rule' => 'notBlank',
+	    'message' => 'El nombre del vehículo no puede estar vacío'
+	),
+	'cantidad_embalaje' => array(
+	    'rule' => 'notBlank',
+	    'message' => 'La cantidad de bultos no puede estar vacía'
+	),
+	'linea' => array(
+	    'rule' => 'notBlank',
+	    'message' => 'La linea de transporte no puede estar vacía'
+	)
     );
 
     public $belongsTo = array(
@@ -43,7 +42,5 @@ class Transporte extends AppModel {
 	    'className' => 'AlmacenTransporte',
 	    'foreignKey' => 'transporte_id')
 	);
-
-
 }
 ?>
