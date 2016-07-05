8dec235c57a4e45c882b3b95f0b48498b8b35293
Eric Van Buggenhaut <ericvb69@gmail.com>
2016-07-05 19:00:35 +0200

    Metiendo el diferencial en Operacion::add()

--- app/Model/Agente.php
+++ app/Model/Agente.php
@@ -1,16 +1,11 @@
 <?php
 class Agente extends AppModel{
-  public $displayfield = 'nombre_corto';
-  public $recursive = 2;
-  public $hasOne = array('Empresa' => array(
-	'className' => 'Empresa',
-	'foreignKey' => 'id')
-  );
-  //public $belongsTo = 'Pais';
-  //public $validate = array(
-  //  'nombre' => array(
-  //    'rule' => 'notBlank',
-  //    'message' => 'El nombre no puede estar vacio'
-  //    )
-  //);
+	public $displayfield = 'nombre_corto';
+	public $recursive = 2;
+	public $hasOne = array(
+		'Empresa' => array(
+			'className' => 'Empresa',
+			'foreignKey' => 'id'
+		)
+	);
 }
--- app/Model/AlmacenTransporte.php
+++ app/Model/AlmacenTransporte.php
@@ -1,13 +1,15 @@
 <?php
 class AlmacenTransporte extends AppModel {
 	public $displayField = 'cuenta_almacen';
-    public $belongsTo = array(
-	'Almacen' => array(
-	    'className' => 'Empresa',
-	    'foreignKey' => 'almacen_id'),
-	'Transporte' => array(
-	    'className' => 'Transporte',
-	    'foreignKey' => 'transporte_id')
+	public $belongsTo = array(
+		'Almacen' => array(
+			'className' => 'Empresa',
+			'foreignKey' => 'almacen_id'
+		),
+		'Transporte' => array(
+			'className' => 'Transporte',
+			'foreignKey' => 'transporte_id'
+		)
 	);
 	public $hasMany = array(
 		'Retirada',
@@ -15,29 +17,29 @@ class AlmacenTransporte extends AppModel {
 		'AlmacenReparto' => array(
 			'className' => 'AlmacenReparto',
 			'foreignKey' => 'id'
-			)
-		);
-    public $virtualFields = array(
-    'cuenta_marca' => 'CONCAT(AlmacenTransporte.cuenta_almacen," (",COALESCE(AlmacenTransporte.marca_almacen,""),")")'
-);
+		)
+	);
+	public $virtualFields = array(
+		'cuenta_marca' => 'CONCAT(AlmacenTransporte.cuenta_almacen," (",COALESCE(AlmacenTransporte.marca_almacen,""),")")'
+	);
 	public $validate = array(
-    	'almacen_id' => array(
-	      	'rule' => 'notBlank',
-	     	'message' => 'El nombre del almacén no puede estar vacío'
-      	),
-	    'cuenta_almacen' => array(
-      		'rule' => 'notBlank',
-     		'message' => 'La referencia no puede estar vacía'
-      		)
-    );
+		'almacen_id' => array(
+			'rule' => 'notBlank',
+			'message' => 'El nombre del almacén no puede estar vacío'
+		),
+		'cuenta_almacen' => array(
+			'rule' => 'notBlank',
+			'message' => 'La referencia no puede estar vacía'
+		)
+	);
 	public function beforeDelete($cascade = true) {
 		$count = $this->Retirada->find(
-            "count",
-            array(
-                "recursive" => -1,
-                "conditions" => array("almacen_transporte_id" => $this->id)
-            )
-        );
+			"count",
+			array(
+				"recursive" => -1,
+				"conditions" => array("almacen_transporte_id" => $this->id)
+			)
+		);
 		if ($count == 0) {
 			return true;
 		}
